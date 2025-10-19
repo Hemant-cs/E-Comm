34480dc8bfe32a63cf538bc4ce0f6c948a80618c
Hemant Chaurasia <hemantchaurasia555@gmail.com>
2025-10-19 20:01:12 +0530

    added db connection

---
+++ backend/DB/Products.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+//for connecting to db
+require("./ConnectDb");
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true },
+    price: { type: Number, required: true, min: 0 },
+    brand: { type: String, required: true, trim: true },
+    category: { type: String, required: true, trim: true }
+}, { timestamps: true });
+
+module.exports = mongoose.models.products || mongoose.model("products", ProductSchema);
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const Product = require("./DB/Products");
 
-app.get("/", (req, res)=>{
-    res.send("your backend is working....");
+app.get("/", async(req, res)=>{
+    // const allData = await Product.find();
+    const result = await Product.find();
+    res.send(result);
 });
 
 app.listen(8080);
